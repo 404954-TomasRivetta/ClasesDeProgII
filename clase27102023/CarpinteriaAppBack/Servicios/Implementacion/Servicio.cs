39a412b5ec70ea7692d13df1d78d645b936fad23
404954-TomasRivetta <404954@tecnicatura.frc.utn.edu.ar>
2023-10-30 18:08:04 -0300

    agregando clase del 27 de octubre

---
+++ clase27102023/CarpinteriaAppBack/Servicios/Implementacion/Servicio.cs
@@ -0,0 +1,35 @@
+﻿using CarpinteriaBack.Datos.Implementacion;
+using CarpinteriaBack.Datos.Interfaz;
+using CarpinteriaBack.Entidades;
+using CarpinteriaBack.Servicios.Interfaz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarpinteriaBack.Servicios.Implementacion
+{
+    public class Servicio : IServicio
+    {
+        private IPresupuestoDao dao;
+        public Servicio()
+        {
+            dao = new PresupuestoDao();
+        }
+        public bool CrearPresupuesto(Presupuesto oPresupuesto)
+        {
+           return dao.Crear(oPresupuesto);
+        }
+
+        public List<Producto> TraerProductos()
+        {
+            return dao.ObtenerProductos();
+        }
+
+        public int TraerProximoPresupuesto()
+        {
+            return dao.ObtenerProximoPresupuesto(); 
+        }
+    }
+}
---
+++ clase27102023/CarpinteriaAppBack/Servicios/Interfaz/IServicio.cs
@@ -0,0 +1,17 @@
+﻿using CarpinteriaBack.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarpinteriaBack.Servicios.Interfaz
+{
+    public interface IServicio
+    {
+        int TraerProximoPresupuesto();
+        List<Producto> TraerProductos();
+        bool CrearPresupuesto(Presupuesto oPresupuesto);
+
+    }
+}
